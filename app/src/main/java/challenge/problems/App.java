77b9b8cd0142682dec9dc7a6b7557de16f02b809
ruuffian <ruuffian@gmail.com>
2022-05-31 20:17:13 -0400

    target array solved for first solution

--- app/src/main/java/challenge/problems/App.java
+++ app/src/main/java/challenge/problems/App.java
@@ -4,11 +4,8 @@
 package challenge.problems;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        TargetSubset.main();
     }
 }
--- app/src/main/java/challenge/problems/TargetSubset.java
+++ app/src/main/java/challenge/problems/TargetSubset.java
@@ -2,46 +2,49 @@ package challenge.problems;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
-public class TargetArraySum {
-    public static void main(String[] args) {
-        long target = 5;
+public class TargetSubset {
 
-        String setString = "2,3,4";
+    public static void main() {
+        long target = 24;
+
+        String setString = "1,3,7,13";
         String[] splitSet = setString.split(",");
         Long[] set = Arrays.stream(splitSet).map(Long::parseLong).toArray(Long[]::new);
 
-        System.out.println((new TargetArraySum()).recurse(set, target).subset);
+        ArrayList<Long> solution = (new TargetSubset()).recurse(set, target).subset;
+        Collections.sort(solution);
+        System.out.println("Base Set:: " + Arrays.toString(set));
+        System.out.println("Target:: " + target);
+        System.out.println("Solution:: " + solution);
     }
 
     public SubsetState recurse(Long[] set, long target) {
         SubsetState state = new SubsetState(set, target, new ArrayList<>());
-        long element = 0;
-        for (int i = 0; i < set.length && set[i] < target; i++) {
-            element = set[i];
-        }
-        return recurse(state, element);
+        SubsetState solution = recurse(state, 0);
+        if (solution.subset.stream().mapToLong(Long::longValue).sum() != target)
+            return (new SubsetState(set, target, new ArrayList<>()));
+        else return solution;
     }
 
     public SubsetState recurse(SubsetState state, long element) {
-        if (state.target == 0) {
-            return state;
-        } else if(state.target < 0){
-            state.subset.remove(element);
+        if (state.target <= 0) {
             return state;
         }
 
         int el_index = 0;
-        while (el_index < state.set.length && state.set[el_index] < state.target) {
+        while (el_index < state.set.length - 1 && state.set[el_index] < state.target) {
             el_index++;
         }
-        el_index--;
         while (el_index >= 0) {
             long new_el = state.set[el_index];
             state.addToSolution(new_el);
             state = recurse(state, new_el);
             if (state.target == 0) {
                 return state;
+            } else {
+                state.removeFromSolution(element);
             }
             el_index--;
         }
@@ -59,7 +62,14 @@ class SubsetState {
         this.target = target;
         this.subset = subset;
     }
-    public void addToSolution(long element){
+
+    public void addToSolution(long element) {
         this.subset.add(element);
+        this.target -= element;
+    }
+
+    public void removeFromSolution(long element) {
+        this.subset.remove(element);
+        this.target += element;
     }
 }
